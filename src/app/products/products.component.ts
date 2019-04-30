0336d4a89173f3f3712970d644d36c32bc03b24e
mossiXYZ <mossi.hashemi@gmail.com>
2019-04-30 20:03:45 +0200

    322 Dealing with Multiple Asynchronous Operations

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,6 +3,7 @@ import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
@@ -22,19 +23,22 @@ export class ProductsComponent {
     private productService: ProductService,
     private categoryService: CategoryService
   ) {
-    productService.getAll().subscribe(products => this.products = products);
+    productService
+      .getAll()
+      .switchMap(products => {
+        this.products = products;
+        return route.queryParamMap;
+      })
+      .subscribe(params => {
+        this.category = params.get('category');
+
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
+
     this.categories$ = categoryService.getAll();
 
-
-    route.queryParamMap.subscribe(params => {
-      this.category = params.get('category');
-
-      this.filteredProducts = (this.category) ?
-        this.products.filter(p => p.category === this.category) :
-        this.products;
-    });
   }
-
-
 
 }
