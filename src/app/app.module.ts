21fcf3653c4b037b51023e8b281ec9500039e685
mossiXYZ <mossi.hashemi@gmail.com>
2019-04-30 20:16:20 +0200

    323 Refactoring Extracting ProductFilterComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { AdminAuthGaurd } from './admin-auth-gaurd.service';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
 import { CategoryService } from './category.service';
 import { ProductService } from './product.service';
+import { ProductFilterComponent } from './products/product-filter/product-filter.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import { ProductService } from './product.service';
     AdminProductsComponent,
     AdminOrdersComponent,
     LoginComponent,
-    ProductFormComponent
+    ProductFormComponent,
+    ProductFilterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
-import { CategoryService } from '../category.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
 import 'rxjs/add/operator/switchMap';
@@ -14,14 +13,12 @@ export class ProductsComponent {
 
   products: Product[] = [];
   filteredProducts: Product[];
-  categories$;
   category: string;
 
 
   constructor(
     route: ActivatedRoute,
     private productService: ProductService,
-    private categoryService: CategoryService
   ) {
     productService
       .getAll()
@@ -37,7 +34,6 @@ export class ProductsComponent {
           this.products;
       });
 
-    this.categories$ = categoryService.getAll();
 
   }
 
