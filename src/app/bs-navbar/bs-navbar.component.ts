52ad6804ada99bd7dda02536a95019b24c1edbbe
mossiXYZ <mossi.hashemi@gmail.com>
2019-05-03 17:17:13 +0200

    336 Displaying the Number of Shopping Cart Items in NavBar

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { AppUser } from '../models/app-user';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'bs-navbar',
@@ -8,12 +9,22 @@ import { AppUser } from '../models/app-user';
   styleUrls: ['./bs-navbar.component.css']
 })
 
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
 
   appUser: AppUser;
+  shoppingCartItemCount: number;
+  constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) {
 
-  constructor(private auth: AuthService) {
-    auth.appUser$.subscribe(appUser => this.appUser = appUser)
+  }
+
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    let cart$ = await this.shoppingCartService.getCart();
+    cart$.subscribe(cart => {
+      this.shoppingCartItemCount = 0;
+      for (let productId in cart.items)
+        this.shoppingCartItemCount += cart.items[productId].quantity;
+    })
   }
 
   logout() {
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { Product } from './models/product';
 import 'rxjs/add/operator/take';
+import { ShoppingCart } from './models/shopping-cart';
 
 @Injectable()
 export class ShoppingCartService {
@@ -15,7 +16,7 @@ export class ShoppingCartService {
     })
   }
 
-  async getCart() {
+  async getCart(): Promise<FirebaseObjectObservable<ShoppingCart>> {
     let cartId = await this.getorCreateCartId();
     return this.db.object('/shopping-carts/' + cartId)
   }
