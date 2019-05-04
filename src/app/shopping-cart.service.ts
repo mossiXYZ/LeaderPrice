48a07ca81057639177fdcd28a7f3f31fa78b4395
mossiXYZ <mossi.hashemi@gmail.com>
2019-05-04 12:26:32 +0200

    337 Refactoring & creating ShoppingCart Class

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { Product } from './models/product';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
 import { ShoppingCart } from './models/shopping-cart';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ShoppingCartService {
@@ -16,9 +18,10 @@ export class ShoppingCartService {
     })
   }
 
-  async getCart(): Promise<FirebaseObjectObservable<ShoppingCart>> {
+  async getCart(): Promise<Observable<ShoppingCart>> {
     let cartId = await this.getorCreateCartId();
     return this.db.object('/shopping-carts/' + cartId)
+      .map(x => new ShoppingCart(x.items));
   }
 
   private async getorCreateCartId(): Promise<string> {
