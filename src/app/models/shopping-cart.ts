43a79492aca1bb9172534ef24262788babea2cfd
mossiXYZ <mossi.hashemi@gmail.com>
2019-05-04 14:43:11 +0200

    340 Displaying the Total Price

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -2,12 +2,14 @@ import { ShoppingCartItem } from "./shopping-cart-item";
 
 export class ShoppingCart {
 
-    constructor(public items: ShoppingCartItem[]) {
+    items: ShoppingCartItem[] = [];
 
-    }
+    constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+        for (let productId in itemsMap) {
+            let item = itemsMap[productId];
+            this.items.push(new ShoppingCartItem(item.product, item.quantity));
 
-    get productIds() {
-        return Object.keys(this.items);
+        }
     }
 
     get totalItemsCount() {
@@ -15,5 +17,12 @@ export class ShoppingCart {
         for (let productId in this.items)
             count += this.items[productId].quantity;
         return count;
+    }
+
+    get totalPrice() {
+        let sum = 0;
+        for (let productId in this.items)
+            sum += this.items[productId].totalPrice;
+        return sum;
     }
 }
