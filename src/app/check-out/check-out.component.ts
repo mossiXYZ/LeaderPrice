e3f1025e3e085ac4bae183d07e3082cc9696c904
mossiXYZ <mossi.hashemi@gmail.com>
2019-05-07 11:33:06 +0200

    352 Building a Shipping Form

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent {
 
-  constructor() { }
+  shipping = {};
 
-  ngOnInit() {
+  placeOrder() {
+    console.log(this.shipping);
   }
-
 }
